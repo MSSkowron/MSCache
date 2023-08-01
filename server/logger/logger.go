e434e1dd8238f174352b3e50268fb35cf39a9eeb
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-08-01 12:35:23 +0200

    Refactor logger's code

--- server/logger/logger.go
+++ server/logger/logger.go
@@ -7,19 +7,14 @@ import (
 
 var CustomLogger *Logger
 
-func init() {
-	CustomLogger = newLogger()
-}
-
 // Logger represents a custom logger with separate info and error loggers.
 type Logger struct {
 	Info  *log.Logger
 	Error *log.Logger
 }
 
-// newLogger creates a new instance of Logger.
-func newLogger() *Logger {
-	return &Logger{
+func init() {
+	CustomLogger = &Logger{
 		Info:  log.New(os.Stdout, "[INFO] ", log.Ldate|log.Ltime|log.Lshortfile),
 		Error: log.New(os.Stdout, "[ERROR] ", log.Ldate|log.Ltime|log.Lshortfile),
 	}
