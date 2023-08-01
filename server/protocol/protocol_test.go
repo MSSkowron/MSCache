9c44e1e7eb37eee1866d466ec5ba56af53b21c8e
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-08-01 19:29:56 +0200

    Add test cases for protocol package

--- server/protocol/protocol_test.go
+++ server/protocol/protocol_test.go
@@ -7,52 +7,145 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBytes(t *testing.T) {
-	cmdValid := &CommandSet{
-		Key:   []byte("Foo"),
-		Value: []byte("Bar"),
-		TTL:   2,
+func TestResponseSetBytes(t *testing.T) {
+	respValid := &ResponseSet{
+		Status: StatusOK,
 	}
 
-	_, err := cmdValid.Bytes()
-	assert.Nil(t, err)
+	b, err := respValid.Bytes()
+	assert.NoError(t, err)
+
+	expected := []byte{0x1} // StatusOK = 1
+	assert.Equal(t, expected, b)
 }
 
-func TestParseSetCommand(t *testing.T) {
-	cmd := &CommandSet{
-		Key:   []byte("Foo"),
-		Value: []byte("Bar"),
-		TTL:   2,
+func TestResponseSetParse(t *testing.T) {
+	resp := &ResponseSet{
+		Status: StatusOK,
 	}
 
-	b, err := cmd.Bytes()
-	assert.Nil(t, err)
+	b, err := resp.Bytes()
+	assert.NoError(t, err)
 
 	r := bytes.NewReader(b)
 
-	pcmd, err := ParseCommand(r)
-	assert.Nil(t, err)
+	presp, err := ParseSetResponse(r)
+	assert.NoError(t, err)
 
-	assert.Equal(t, cmd, pcmd)
+	assert.Equal(t, resp, presp)
 }
 
-func TestParseGetCommand(t *testing.T) {
+func TestResponseGetBytes(t *testing.T) {
+	respValid := &ResponseGet{
+		Status: StatusOK,
+		Value:  []byte("Bar"),
+	}
+
+	b, err := respValid.Bytes()
+	assert.NoError(t, err)
+
+	expected := []byte{0x1, 0x3, 0x0, 0x0, 0x0, 0x42, 0x61, 0x72}
+
+	assert.Equal(t, expected, b)
+}
+
+func TestResponseGetParse(t *testing.T) {
+	resp := &ResponseGet{
+		Status: StatusOK,
+		Value:  []byte("Bar"),
+	}
+
+	b, err := resp.Bytes()
+	assert.NoError(t, err)
+
+	r := bytes.NewReader(b)
+
+	presp, err := ParseGetResponse(r)
+	assert.NoError(t, err)
+
+	assert.Equal(t, resp, presp)
+}
+
+func TestResponseDeleteBytes(t *testing.T) {
+	respValid := &ResponseDelete{
+		Status: StatusOK,
+	}
+
+	b, err := respValid.Bytes()
+	assert.NoError(t, err)
+
+	expected := []byte{0x01} // StatusOK = 1
+	assert.Equal(t, expected, b)
+}
+
+func TestResponseDeleteParse(t *testing.T) {
+	resp := &ResponseDelete{
+		Status: StatusOK,
+	}
+
+	b, err := resp.Bytes()
+	assert.NoError(t, err)
+
+	r := bytes.NewReader(b)
+
+	presp, err := ParseDeleteResponse(r)
+	assert.NoError(t, err)
+
+	assert.Equal(t, resp, presp)
+}
+
+func TestCommandGetBytes(t *testing.T) {
+	cmdValid := &CommandGet{
+		Key: []byte("Foo"),
+	}
+
+	b, err := cmdValid.Bytes()
+	assert.NoError(t, err)
+
+	expected := []byte{
+		0x02,                   // CmdGet = 2
+		0x03, 0x00, 0x00, 0x00, // length of "Foo" in little-endian format
+		0x46, 0x6F, 0x6F, // ASCII values for "Foo"
+	}
+	assert.Equal(t, expected, b)
+}
+
+func TestCommandGetParse(t *testing.T) {
 	cmd := &CommandGet{
 		Key: []byte("Foo"),
 	}
 
 	b, err := cmd.Bytes()
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 
 	r := bytes.NewReader(b)
 
 	pcmd, err := ParseCommand(r)
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 
-	assert.Equal(t, cmd, pcmd)
+	pcmdGet, ok := pcmd.(*CommandGet)
+	assert.True(t, ok)
+
+	assert.Equal(t, cmd, pcmdGet)
 }
 
-func TestParseDeleteCommand(t *testing.T) {
+func TestCommandDeleteBytes(t *testing.T) {
+	cmdValid := &CommandDelete{
+		Key: []byte("Foo"),
+	}
+
+	b, err := cmdValid.Bytes()
+	assert.NoError(t, err)
+
+	expected := []byte{
+		0x03,                   // CmdDel = 3
+		0x03, 0x00, 0x00, 0x00, // length of "Foo" in little-endian format
+		0x46, 0x6F, 0x6F, // ASCII values for "Foo"
+	}
+	assert.Equal(t, expected, b)
+}
+
+func TestCommandDeleteParse(t *testing.T) {
 	cmd := &CommandDelete{
 		Key: []byte("Foo"),
 	}
@@ -65,5 +158,8 @@ func TestParseDeleteCommand(t *testing.T) {
 	pcmd, err := ParseCommand(r)
 	assert.Nil(t, err)
 
-	assert.Equal(t, cmd, pcmd)
+	pcmdDel, ok := pcmd.(*CommandDelete)
+	assert.True(t, ok)
+
+	assert.Equal(t, cmd, pcmdDel)
 }
